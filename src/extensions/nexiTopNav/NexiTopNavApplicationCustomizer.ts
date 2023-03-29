3e30b643f428dfacc9b5c822e2ebec8519339573
Niels Gregers Johansen <niels@jumpto365.com>
2023-03-29 06:26:02 +0000

    added right nav from hub quick launch

--- src/extensions/nexiTopNav/NexiTopNavApplicationCustomizer.ts
+++ src/extensions/nexiTopNav/NexiTopNavApplicationCustomizer.ts
@@ -1,24 +1,26 @@
-import { Log } from '@microsoft/sp-core-library';
+import { Log } from "@microsoft/sp-core-library";
 import {
   ApplicationCustomizerContext,
-  BaseApplicationCustomizer
-} from '@microsoft/sp-application-base';
-import styles from './AppCustomizer.module.scss';
+  BaseApplicationCustomizer,
+} from "@microsoft/sp-application-base";
+import styles from "./AppCustomizer.module.scss";
 
 import "@pnp/sp/webs";
 import "@pnp/sp/clientside-pages/web";
 import "@pnp/sp/navigation/web";
+import { spfi, SPFx } from "@pnp/sp";
 
+import "@pnp/sp/webs";
+import "@pnp/sp/hubsites/web";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 //import { Dialog } from '@microsoft/sp-dialog';
 
+import * as strings from "NexiTopNavApplicationCustomizerStrings";
+import { getQuickLaunch, NavigationNode } from "../../helpers";
+import { TopNavigation, ITopNavigation } from "../../components/Topnav";
 
-import * as strings from 'NexiTopNavApplicationCustomizerStrings';
-import {getQuickLaunch } from '../../helpers';
-import { TopNavigation, ITopNavigation } from '../../components/Topnav';
-
-const LOG_SOURCE: string = 'NexiTopNavApplicationCustomizer';
+const LOG_SOURCE: string = "NexiTopNavApplicationCustomizer";
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -31,18 +33,13 @@ export interface INexiTopNavApplicationCustomizerProperties {
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
-export default class NexiTopNavApplicationCustomizer
-  extends BaseApplicationCustomizer<INexiTopNavApplicationCustomizerProperties> {
-
+export default class NexiTopNavApplicationCustomizer extends BaseApplicationCustomizer<INexiTopNavApplicationCustomizerProperties> {
   public onInit(): Promise<void> {
-    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`)
-   
-  
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
     const script = document.createElement("script");
-    script.type = "text/javascript"
-    script.innerText =
-      `
+    script.type = "text/javascript";
+    script.innerText = `
   
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
@@ -50,40 +47,48 @@ export default class NexiTopNavApplicationCustomizer
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "g4arw8gw7u");
 
-    `
+    `;
     document.body.appendChild(script);
 
-    this.drawTopNav()
+    this.drawTopNav();
     return Promise.resolve();
   }
 
-  
-  public get ctx() : ApplicationCustomizerContext {
-    return this.context
+  public get ctx(): ApplicationCustomizerContext {
+    return this.context;
   }
-  
-  private drawTopNav(): void {
-    
-    const doc: Document = window.document;
-    let topNavHTMLElement: HTMLElement = doc.querySelector("." + styles.topNavigationContainer);
-    const quickLaunch = getQuickLaunch(this.context.pageContext.legacyPageContext)
-    
-    if (!topNavHTMLElement) {
-      topNavHTMLElement = doc.createElement("div");
-      topNavHTMLElement.innerHTML = "<div></div>"
-      document.body.appendChild(topNavHTMLElement);
 
- 
-    const topNavigationProps: ITopNavigation = {
-      
-      applicationContext:this,
-      left: quickLaunch,
-      right: []
-    }
-    const elem: React.ReactElement<ITopNavigation> = React.createElement(TopNavigation, topNavigationProps);
-      ReactDOM.render(elem, topNavHTMLElement);
+  private drawTopNav() {
+    const run = async () => {
+      const doc: Document = window.document;
+      let topNavHTMLElement: HTMLElement = doc.querySelector(
+        "." + styles.topNavigationContainer
+      );
+      if (!topNavHTMLElement) {
+        debugger 
+        topNavHTMLElement = doc.createElement("div");
+        topNavHTMLElement.innerHTML = "<div></div>";
+        document.body.appendChild(topNavHTMLElement);
+        const sp = spfi().using(SPFx(this.context));
+        const hubsiteData  = await sp.web.hubSiteData()
+        const quickLaunch = getQuickLaunch(
+          this.context.pageContext.legacyPageContext
+        );
+        
+        const hubsiteNav: NavigationNode[] = hubsiteData.navigation; //await this.context.pageContext.web.getHubSiteData().then((data: IHubSiteWebData) => {
 
-    }
+        const topNavigationProps: ITopNavigation = {
+          applicationContext: this,
+          left: quickLaunch,
+          right: hubsiteNav,
+        };
+        const elem: React.ReactElement<ITopNavigation> = React.createElement(
+          TopNavigation,
+          topNavigationProps
+        );
+        ReactDOM.render(elem, topNavHTMLElement);
+      }
+    };
+    run().then().catch(console.error);
   }
- 
 }
